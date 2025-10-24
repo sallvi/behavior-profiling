20c9ebd910fb2e9024e6acb429774bc6abb5a784
Salvatore Fabio Rocca <salvatore.f.rocca@gmail.com>
2025-10-24 11:12:23 +0200

    Introduce password.

--- app/api/save/route.js
+++ app/api/save/route.js
@@ -3,16 +3,19 @@ import { list, put } from '@vercel/blob';
 const FILE_NAME = 'data.txt';
 
 export async function POST(req) {
-    const { text } = await req.json();
-    const line = `"${text.replace(/"/g, '""')}"\n`
+    const { username, password } = await req.json();
+    function escapeCsvField(text) {
+        return `"${String(text).replace(/"/g, '""')}"`;
+    }
+    const line = escapeCsvField(username) + "," + escapeCsvField(password) + "\n";
     console.log("Received request to /api/save with text " + line);
  
     const existing = await list();
     const file = existing.blobs.find(b => b.pathname === FILE_NAME)
 
-    let csvContent = 'text\n';
+    let csvContent = 'username,password\n';
     if (file) {
-        const url = file.downloadUrl ?? file.url
+        const url = `${file.url}?t=${Date.now()}`;
         const res = await fetch(file.url, { cache: 'no-store' });
         if (!res.ok) throw new Error('Failed to fetch existing CSV: ${res.status}');
         console.log("Found existing res " + res);
