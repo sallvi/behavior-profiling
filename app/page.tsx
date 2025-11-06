5dc5863cc66c0db8e2d4839253e8317572e89f9e
sallvi <salvatore.f.rocca@gmail.com>
2025-11-06 14:43:06 +0100

    Change heading text for login form

--- app/page.tsx
+++ app/page.tsx
@@ -367,7 +367,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="text-4xl font-bold mb-8">Enter your fake login data</h1>
+        <h1 className="text-4xl font-bold mb-8">Enter your login data</h1>
         <form onSubmit={handleSubmit}>
           <div>
             <input 
